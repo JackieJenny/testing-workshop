f5fc309e0690c0dff7145047a3f3094551f0862c
Jack Xu <jack2005nz@gmail.com>
2024-01-10 21:23:16 +0000

    sorry i need to commit more  -zac

--- other/whats-a-mock/__tests__/thumb-war.1.todo.js
+++ other/whats-a-mock/__tests__/thumb-war.1.todo.js
@@ -4,17 +4,22 @@
 
 import * as utils from '../utils'
 
-// monkey-patching
+// monkey-patching   (is required sometimes but is rarely ideal. better not to mock because if another prop is added it still passes even though its broken)
 import thumbWar from '../thumb-war'
 
 describe('Thumbwar', () => {
   it('Should return the winner', () => {
     const originalGetWinner = utils.getWinner //saving the original winner to be user for other tests
 
-    utils.getWinner = function (p1, p2) {
-      //monkey patching to force p1 to be winner
+    utils.getWinner = (p1, p2) => {
       return p1
     }
+
+    // //other way of writing function
+    // utils.getWinner = function (p1, p2) {
+    //   //monkey patching to force p1 to be winner
+    //   return p1
+    // }
     const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds')
     expect(winner).toBe('Ken Wheeler')
 
--- other/whats-a-mock/__tests__/thumb-war.2.todo.js
+++ other/whats-a-mock/__tests__/thumb-war.2.todo.js
@@ -1,24 +1,22 @@
 // improved assertions for mocks
+import {util} from 'prettier'
 import thumbWar from '../thumb-war'
 import * as utils from '../utils'
 
 test('returns winner', () => {
   const originalGetWinner = utils.getWinner
-  // change the getWinner implementation to a function
-  // that keeps track of how often it's called and
-  // the arguments it's called with (Hint #1)
-  utils.getWinner = (p1, p2) => p2
+
+  utils.getWinner = (...args) => {
+    utils.getWinner
+  }
+
+  utils.getWinner.mock = {calls: []}
 
   const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds')
   expect(winner).toBe('Kent C. Dodds')
-  // add an assertion for how many times the getWinner function
-  // was supposed to be called (2 times) (Hint #2)
-  //
-  // add another assertion that every time it was called
-  // it was called with the right arguments: 'Ken Wheeler', 'Kent C. Dodds'
-  // (Hint #3)
+  expect.any(string)
 
-  utils.getWinner = originalGetWinner
+  utils.getWinner = originalGetWinner //IMOPORTANT
 })
 
 /*
